59d3e752acf9e20e9dbc0818566c70aa79d535ba
ronnyvan <115511513+ronnyvan@users.noreply.github.com>
2023-10-30 16:16:46 -0500

    Add files via upload

---
+++ wpms.py
@@ -0,0 +1,33 @@
+import random
+import time
+import pyautogui
+
+
+time.sleep(2)
+text = open("./text.txt", 'r') 
+for line in text:
+    words = line.split()
+    for word in words:
+        #the lower the interval the shorter the duration between each keystroke
+        # -> faster time to type that word
+        # range 0.10 to 0.60 replicates above average typespeed ~50-60 wpm
+        # increase the upper bound to type slower
+        # you can increase the lower bound if you want
+
+        pyautogui.typewrite(word, interval=round(random.uniform(0.1, 0.6), 2))
+        #randomized time between each word
+        time.sleep(len(word)*0.1)
+        pyautogui.press('space')
+        # 1/12 chance to replicate a 'break' or pause for 5 to 15 seconds
+        if random.randint(1, 5) == 1:
+            randInterval = random.randint(5, 15)
+            print(f"pause for {randInterval}")
+            time.sleep(randInterval)
+
+
+
+# keyboard.type("wsg gang")
+
+# text = open("./text.txt")
+# for each_line in text:
+#     keyboard.type(each_line)
